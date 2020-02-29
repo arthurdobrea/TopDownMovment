f4c73abaf9bad5b0a01021dcc8c77ebd1ceb20f1
arthurdobrea <arthurdobrya@gmail.com>
2020-02-29 18:29:41 +0200

    adding basic movement

---
+++ Assets/CustomPlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPlayerController : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Camera camera;
+    public Plane plane;
+    public CharacterController characterController;
+
+    private float moveSpeed = 3;
+    private Vector3 finalVelocity;
+    private Animator animator;
+    private Vector3 input;
+    private Vector3 correction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>(); 
+        plane = new Plane(Vector3.up,Vector3.zero);
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    { 
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        finalVelocity = input.normalized * moveSpeed;
+        
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        float rayDistance;
+        
+        if (plane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin,point,Color.red);
+            lookAt(point);
+        }
+    }
+
+    void lookAt(Vector3 point)
+    { 
+        correction = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(correction);
+    }
+
+    void FixedUpdate()
+    {
+        characterController.Move(finalVelocity * Time.deltaTime);
+    }
+}
---
+++ Assets/MovementPlayer.cs
@@ -0,0 +1,48 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// public class MovementPlayer : MonoBehaviour
+// {
+//     private float normalSpeed = 4;
+//     private float runSpeed = 7;
+//     private float rotationSpeed = 80;
+//     private float rot = 0;
+//     private float gravity = 8;
+//
+//     private Vector3 moveDir = Vector3.zero;
+//     public CharacterController characterController;
+//
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         characterController = GetComponent<CharacterController>();
+//     }
+//
+//     private Vector3 move()
+//     {
+//         return new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")) * normalSpeed;
+//         
+//     }
+//
+//     private Vector3 calculateVectorOfMOveDirection(Vector3 direction)
+//     {
+//         moveDir = direction;
+//         moveDir *= normalSpeed;
+//         moveDir = transform.TransformDirection(moveDir);
+//         return moveDir;
+//     }
+//
+//     private void moveCharachter(Vector3 moveDir)
+//     {
+//         rot += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+//         transform.eulerAngles = new Vector3(0, rot, 0);
+//         moveDir.y -= gravity * Time.deltaTime;
+//         characterController.Move(moveDir * Time.deltaTime);
+//     }
+//
+//     void FixedUpdate()
+//     {
+//         moveCharachter(move());
+//     }
+// }
