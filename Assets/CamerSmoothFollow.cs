82d34871567b5a41a0bf75afb9fc8a03b10aa6bd
arthurdobrea <arthurdobrya@gmail.com>
2020-03-01 10:54:39 +0200

    Adding basic lvl and camera follow script

---
+++ Assets/CamerSmoothFollow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerSmoothFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        
+        transform.LookAt(player.transform);
+    }
+}
--- Assets/CustomPlayerController.cs
+++ Assets/CustomPlayerController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CustomPlayerController : MonoBehaviour
 {
@@ -9,40 +7,67 @@ public class CustomPlayerController : MonoBehaviour
     public Plane plane;
     public CharacterController characterController;
 
-    private float moveSpeed = 3;
+    public float finalSpeed;
+    public float runSpeed = 3;
+    public float moveSpeed = 1.5f;
     private Vector3 finalVelocity;
-    private Animator animator;
     private Vector3 input;
     private Vector3 correction;
+    private float gravity = -16;
+    private Actions actions;
+    private float run = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>(); 
-        plane = new Plane(Vector3.up,Vector3.zero);
+        rigidbody = GetComponent<Rigidbody>();
+        plane = new Plane(Vector3.up, Vector3.zero);
         characterController = GetComponent<CharacterController>();
+        actions = GetComponent<Actions>();
     }
 
     void Update()
-    { 
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        finalVelocity = input.normalized * moveSpeed;
-        
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                finalSpeed = runSpeed;
+                actions.Run();
+            }
+            else
+            {
+                finalSpeed = moveSpeed;
+                actions.Walk();
+            }
+        }
+
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) ||
+                 Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.C))
+        {
+            actions.Stay();
+        }
+
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), gravity * Time.deltaTime, Input.GetAxisRaw("Vertical"));
+        finalVelocity = input.normalized * finalSpeed;
+
+
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         float rayDistance;
-        
+
         if (plane.Raycast(ray, out rayDistance))
         {
             Vector3 point = ray.GetPoint(rayDistance);
-            Debug.DrawLine(ray.origin,point,Color.red);
+            Debug.DrawLine(ray.origin, point, Color.red);
             lookAt(point);
         }
     }
 
     void lookAt(Vector3 point)
-    { 
+    {
         correction = new Vector3(point.x, transform.position.y, point.z);
         transform.LookAt(correction);
+        transform.Rotate(0, Input.GetAxisRaw("Horizontal"), 0);
     }
 
     void FixedUpdate()
