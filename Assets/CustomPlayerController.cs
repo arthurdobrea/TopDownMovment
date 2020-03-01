fa9dc8175f87b2dd9e7644d58f21a467831e1685
arthurdobrea <arthurdobrya@gmail.com>
2020-03-01 11:38:55 +0200

    refactor

--- Assets/CustomPlayerController.cs
+++ Assets/CustomPlayerController.cs
@@ -10,7 +10,7 @@ public class CustomPlayerController : MonoBehaviour
     public float finalSpeed;
     public float runSpeed = 3;
     public float moveSpeed = 1.5f;
-    private Vector3 finalVelocity;
+    private Vector3 finalDirection;
     private Vector3 input;
     private Vector3 correction;
     private float gravity = -16;
@@ -25,33 +25,22 @@ public class CustomPlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         actions = GetComponent<Actions>();
     }
-
+    
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                finalSpeed = runSpeed;
-                actions.Run();
-            }
-            else
-            {
-                finalSpeed = moveSpeed;
-                actions.Walk();
-            }
-        }
-
-        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) ||
-                 Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.C))
-        {
-            actions.Stay();
-        }
-
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), gravity * Time.deltaTime, Input.GetAxisRaw("Vertical"));
-        finalVelocity = input.normalized * finalSpeed;
-
-
+        input = new Vector3(Input.GetAxis("Horizontal"), gravity * Time.deltaTime, Input.GetAxis("Vertical"));
+        finalDirection = input.normalized * finalSpeed;
+    }
+    
+    void FixedUpdate()
+    {
+        handleInput();
+        rotateTowardsMouseLocalCoordinate();
+        characterController.Move(finalDirection * Time.deltaTime);
+    }
+    
+    private void rotateTowardsMouseLocalCoordinate()
+    {
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         float rayDistance;
 
@@ -67,11 +56,31 @@ public class CustomPlayerController : MonoBehaviour
     {
         correction = new Vector3(point.x, transform.position.y, point.z);
         transform.LookAt(correction);
-        transform.Rotate(0, Input.GetAxisRaw("Horizontal"), 0);
     }
 
-    void FixedUpdate()
+    private void handleInput()
     {
-        characterController.Move(finalVelocity * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                finalSpeed = runSpeed;
+                actions.Run();
+            }
+            else
+            {
+                finalSpeed = moveSpeed;
+                actions.Walk();
+            }
+        }
+        
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A) ||
+                 Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.C))
+        {
+            actions.Stay();
+        }
     }
+
+    
+
 }
